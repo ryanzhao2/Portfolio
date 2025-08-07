ea5e65f2f384d9855f0f317438da34af24ed0dc1
RyanZhao <ryan.zhao2@gmail.com>
2025-08-07 02:31:41 -0400

    changed url

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  // Ensure assets are copied to the output directory
-  distDir: 'out',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://alvinayang.com' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://ryanzhao.dev' : '',
 }
 
 module.exports = nextConfig 
